d20e88fbd3a5d3b0911d2650c32c7b637695e5b9
Nikolai Stepanov <nikolaistepanov@MacBook-Air-Nikolai.local>
2021-04-15 17:23:10 +0300

    Database is created

---
+++ a_parser/get_data/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class LogsModel(models.Model):
+    ip = models.GenericIPAddressField()
+    data = models.CharField(max_length=25)
+    method = models.CharField(max_length=4)
+    url = models.TextField()
+    response = models.IntegerField()
+    response_size = models.IntegerField()
